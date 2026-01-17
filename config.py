73a95f520293d36d33cbf7f9ae7e91e26178f68d
rajayns42-design <rajayns42@gmail.com>
2026-01-17 09:45:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,26 +13,13 @@ SUPPORT_GRP = getenv("SUPPORT_GRP", "Love_Ki_Duniyaa")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "Love_bot_143")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_WTF_SHEZADA_ll")
 MISTRAL_API_KEY = getenv("MISTRAL_API_KEY", "uPsyariDhh2UwyF5TvgEkWbIhH0HDhFa")
+LOGGER_GROUP_ID = int(getenv("LOGGER_GROUP_ID", "-1003605595874")
+UPI_ID = int(getenv("UPI_ID", "tiwarijiz8900@oksbi")
 
 # Random Start Images
 IMG = [
-    "https://telegra.ph/file/00eb565274ccbffcf149d.jpg",
-    "https://telegra.ph/file/396e2af77c4664164de18.jpg",
-    "https://telegra.ph/file/6f92a0e943d68a15169c0.jpg",
-    "https://telegra.ph/file/d49fbf4c00f839641afe3.jpg",
-    "https://telegra.ph/file/cdcdceb3d4fc34675b815.jpg",
-    "https://telegra.ph/file/cc6259af790c13f98c38c.jpg",
-    "https://telegra.ph/file/ffa18225730df716d3532.jpg",
-    "https://telegra.ph/file/dc051f4160954159675a2.jpg",
-    "https://telegra.ph/file/e0e0a3f2addbf58b943e8.jpg",
-    "https://telegra.ph/file/2e6d9db59c537c4521440.jpg",
-    "https://telegra.ph/file/f1951920bbd57921d8820.jpg",
-    "https://telegra.ph/file/ece475f9a419442c18f1d.jpg",
-    "https://telegra.ph/file/99a0014129f08eb1a44dc.jpg",
-    "https://telegra.ph/file/ac8e3751509cf4e1b4756.jpg",
-    "https://telegra.ph/file/e07ef19b1f9bbde9909ad.jpg",
-    "https://telegra.ph/file/d3ee41261ed7779f30a89.jpg",
-]
+    "https://envs.sh/IL_.jpg",
+    
 
 
 # Random Stickers
